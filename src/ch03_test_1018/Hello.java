181333036b41eb23691203571aba56388400333a
gyuraphy <iraem@kakao.com>
2022-11-24 13:55:47 +0900

    저장 테스트 2

--- src/ch03_test_1018/Hello.java
+++ src/ch03_test_1018/Hello.java
@@ -5,6 +5,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 실행 : ctrl + F11
+		// 주석 : ctrl + /
+		// 여러 행 주석 : ctrl + shift + /
+		// 여러 행 주석 해제 : ctrl + shift + \
+		// 자동 포맷 정령 : ctrl +shift + f
 		System.out.println("Hello 이상용 자바");
 	}
 
