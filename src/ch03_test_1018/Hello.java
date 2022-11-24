0d2e0ee1b8ddf36635c47d1fa62554d600c1c481
gyuraphy <iraem@kakao.com>
2022-11-24 12:01:07 +0900

    수업 예제 저장 221124

---
+++ src/ch03_test_1018/Hello.java
@@ -0,0 +1,11 @@
+package ch03_test_1018;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 실행 : ctrl + F11
+		System.out.println("Hello 이상용 자바");
+	}
+
+}
