a9519db7f46a7bc529d5e8064018ab8e5fa19c0b
gyuraphy <iraem@kakao.com>
2022-12-08 12:49:29 +0900

    221208 4교시 저장

--- src/ch09_abstractex_1020/ComputerTest.java
+++ src/ch09_abstractex_1020/ComputerTest.java
@@ -1,4 +1,4 @@
-package ch9_abstractex_1020;
+package ch09_abstractex_1020;
 
 public class ComputerTest {
 
@@ -6,7 +6,17 @@ public class ComputerTest {
 		//추상 클래스는 인스턴스화 할수 없다. 
 //		Computer c1 = new Computer();
 		Computer c2 = new DeskTop();
+		// c2 바라보는 관점 Computer
+		c2.display();
+		c2.typing();
+		c2.turnOn();
+		c2.turnOff();
+		System.out.println("=========================");
 //		Computer c3 = new NoteBook();
 		Computer c4 = new MyNoteBook();
+		c4.display();
+		c4.typing();
+		c4.turnOn();
+		c4.turnOff();
 	}
 }
