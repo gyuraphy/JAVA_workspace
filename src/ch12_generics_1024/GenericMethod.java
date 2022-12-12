6e4f530cb42c3d12179a5ab78f0d8c3445e8d2a5
gyuraphy <iraem@kakao.com>
2022-12-12 10:09:05 +0900

    221212 1교시 저장

--- src/ch12_generics_1024/GenericMethod.java
+++ src/ch12_generics_1024/GenericMethod.java
@@ -1,7 +1,11 @@
 package ch12_generics_1024;
 
-public class GenericMethod {
+import ch05_test_1018.Animal;
+import ch05_test_1205.Person;
 
+public class GenericMethod {
+	
+	// <T, V> : 매개변수으 ㅣ자료형
 	public static <T, V> double makeRectangle(Point<T, V> p1, Point<T, V> p2) {
 		double left = ((Number)p1.getX()).doubleValue();
 		double left2 = ((Integer)p1.getX()).doubleValue();
@@ -17,9 +21,14 @@ public class GenericMethod {
 	
 	public static void main(String[] args) {
 		
+		// Integer Wrapper 클래스 형, 생성자 호출 없이 기본형 데이터 타입이 바로 들어간 것
+		// 이 Wrapper 클래스의 특징임 자동 형번환, auto boxing, unboxing
+		// Double Wrapper 클래스 형 
 		Point<Integer, Double> p1 = new Point<Integer, Double>(0, 0.0);
 		Point<Integer, Double> p2 = new Point<>(10, 10.0);
 		
+		// 다음 예제로 객체 생성시, 원하는 형으로 선언 가능
+		Point<Person, Animal> p3 = new Point<>(new Person(), new Animal());
 		double rect = GenericMethod.<Integer, Double>makeRectangle(p1, p2);
 		System.out.println("두 점으로 만들어진 사각형의 넓이는 " + rect + "입니다.");
 	}
